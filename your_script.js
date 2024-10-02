4557e2bc085e8ee5e2ce111bf3f5d575eac59f9e
R Script <alanc90@gmail.com>
2024-10-02 09:47:26 -0300

    new stuff

--- your_script.js
+++ your_script.js
@@ -8,16 +8,26 @@ fetch(dataFile)
     document.getElementById('patorating1v1').textContent = Math.max(data.dicopato_1["3"].rating, data.dicopato_2["3"].rating);
     document.getElementById('patoratingtg').textContent = data.dicopato_2["4"].rating;
     document.getElementById('patomatches').textContent = data.dicopato_1["4"].win + data.dicopato_1["4"].losses + data.dicopato_1["3"].win + data.dicopato_1["3"].losses + data.dicopato_2["4"].win + data.dicopato_2["4"].losses + data.dicopato_2["3"].win + data.dicopato_2["3"].losses;
+    document.getElementById('patomatches1v1').textContent = data.dicopato_1["3"].win + data.dicopato_1["3"].losses + data.dicopato_2["3"].win + data.dicopato_2["3"].losses;
+    document.getElementById('patomatchestg').textContent = data.dicopato_1["4"].win + data.dicopato_1["4"].losses + data.dicopato_2["4"].win + data.dicopato_2["4"].losses;
     document.getElementById('nanoxrating1v1').textContent = data.nanox["3"].rating;
     document.getElementById('nanoxratingtg').textContent = data.nanox["4"].rating;
     document.getElementById('nanoxmatches').textContent = data.nanox["4"].win + data.nanox["4"].losses + data.nanox["3"].win + data.nanox["3"].losses;
+    document.getElementById('nanoxmatches1v1').textContent = data.nanox["3"].win + data.nanox["3"].losses;
+    document.getElementById('nanoxmatchestg').textContent = data.nanox["4"].win + data.nanox["4"].losses;
     document.getElementById('sir_monkeyrating1v1').textContent = data.sir_monkey["3"].rating;
     document.getElementById('sir_monkeyratingtg').textContent = data.sir_monkey["4"].rating;
     document.getElementById('sir_monkeymatches').textContent = data.sir_monkey["4"].win + data.sir_monkey["4"].losses + data.sir_monkey["3"].win + data.sir_monkey["3"].losses;
+    document.getElementById('sir_monkeymatches1v1').textContent = data.sir_monkey["3"].win + data.sir_monkey["3"].losses;
+    document.getElementById('sir_monkeymatchestg').textContent = data.sir_monkey["4"].win + data.sir_monkey["4"].losses;
     document.getElementById('alanthekatrating1v1').textContent = Math.max(data.alanthekat["3"].rating, data.carpincho["3"].rating, data.thexcarpincho["3"].rating);
     document.getElementById('alanthekatratingtg').textContent = data.carpincho["4"].rating;
     document.getElementById('alanthekatmatches').textContent = data.alanthekat["4"].win + data.alanthekat["4"].losses + data.alanthekat["3"].win + data.alanthekat["3"].losses + data.carpincho["4"].win + data.carpincho["4"].losses + data.carpincho["3"].win + data.carpincho["3"].losses + data.thexcarpincho["4"].win + data.thexcarpincho["4"].losses + data.thexcarpincho["3"].win + data.thexcarpincho["3"].losses;
+    document.getElementById('alanthekatmatches1v1').textContent = data.alanthekat["3"].win + data.alanthekat["3"].losses + data.carpincho["3"].win + data.carpincho["3"].losses + data.thexcarpincho["3"].win + data.thexcarpincho["3"].losses;
+    document.getElementById('alanthekatmatchestg').textContent = data.alanthekat["4"].win + data.alanthekat["4"].losses + data.carpincho["4"].win + data.carpincho["4"].losses + data.thexcarpincho["4"].win + data.thexcarpincho["4"].losses;
+
   })
+  
   .catch(error => {
     console.error("Error fetching data:", error);
   });
