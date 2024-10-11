1341769662b2d373f217603d242e5fcae1ab6edf
Alan <63990513+athekat@users.noreply.github.com>
2024-10-11 14:37:25 -0300

    Update get_chart.py

--- get_chart.py
+++ get_chart.py
@@ -8,32 +8,18 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 def download_div_image(url, div_selector, filename):
-    """Downloads an image of a specific <div> element from a webpage.
-
-    Args:
-        url: The URL of the webpage.
-        div_selector: The CSS selector for the <div> element.
-        filename: The desired filename for the downloaded image.
-    """
-
-    # Load the webpage using Selenium
-    driver = webdriver.Chrome(options=options)  # Replace with your preferred WebDriver
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
     driver.execute_script("document.body.style.zoom='100%'")
-    # Wait for the <div> element to be visible
-    wait = WebDriverWait(driver, 40)  # Adjust timeout as needed
+    wait = WebDriverWait(driver, 40)
     element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, div_selector)))
     driver.execute_script('arguments[0].scrollIntoView({block: "center"});', element) 
 
-    # Take a screenshot of the <div> element
     element.screenshot(filename)
-
-    # Close the browser
     driver.quit()
 
-# Example usage
 url = "https://ratings.aoe2.se/?team_one=439001-6237950-18660623-6446904&team_two="
-div_selector = "#random_map_histogram"  # Replace with the actual CSS selector
+div_selector = "#random_map_histogram"
 filename = "1v1RM.png"
 
 download_div_image(url, div_selector, filename)
