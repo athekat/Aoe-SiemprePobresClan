99e2aba7ec76c0d5e9a4df67d26e2eaf41286650
Alan <63990513+athekat@users.noreply.github.com>
2024-10-11 03:07:15 -0300

    Update get_chart.py

--- get_chart.py
+++ get_chart.py
@@ -7,8 +7,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument('--headless')
-
 def download_div_image(url, div_selector, filename):
     """Downloads an image of a specific <div> element from a webpage.
 
@@ -23,18 +21,19 @@ def download_div_image(url, div_selector, filename):
     driver.get(url)
 
     # Wait for the <div> element to be visible
-    wait = WebDriverWait(driver, 60)  # Adjust timeout as needed
-    div_element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, div_selector)))
+    wait = WebDriverWait(driver, 40)  # Adjust timeout as needed
+    element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, div_selector)))
+
 
     # Take a screenshot of the <div> element
-    div_element.screenshot(filename)
+    element.screenshot(filename)
 
     # Close the browser
     driver.quit()
 
 # Example usage
 url = "https://ratings.aoe2.se/?team_one=439001-6237950-18660623-6446904&team_two="
-div_selector = "#random_map_histogram"  # Replace with the actual CSS selector
+div_selector = "//*[@id='random_map_histogram']"  # Replace with the actual CSS selector
 filename = "1v1RM.png"
 
 download_div_image(url, div_selector, filename)
