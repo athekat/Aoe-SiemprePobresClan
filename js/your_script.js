063425d45033617c9f24c18d2732125be297d352
athekat <alanc90@gmail.com>
2026-09-16 11:00:13 -0300

    desagrupar players

--- js/your_script.js
+++ js/your_script.js
@@ -6,11 +6,21 @@ const lastUpdated = 'https://raw.githubusercontent.com/athekat/aoe-playersdata/r
 fetch(dataFile)
   .then(response => response.json())
   .then(data => {
-    document.getElementById('patorating1v1').textContent = Math.max(data.dicopato["3"].rating, data.dicopatito["3"].rating, data.pato350z["3"].rating);
-    document.getElementById('patoratingtg').textContent = Math.max(data.dicopato["4"].rating, data.dicopatito["4"].rating, data.pato350z["4"].rating);
-    document.getElementById('patomatches').textContent = data.dicopato["4"].win + data.dicopato["4"].losses + data.dicopato["3"].win + data.dicopato["3"].losses + data.dicopatito["4"].win + data.dicopatito["4"].losses + data.dicopatito["3"].win + data.dicopatito["3"].losses + data.pato350z["4"].win + data.pato350z["4"].losses + data.pato350z["3"].win + data.pato350z["3"].losses;
-    document.getElementById('patomatches1v1').textContent = data.dicopato["3"].win + data.dicopato["3"].losses + data.dicopatito["3"].win + data.dicopatito["3"].losses + data.pato350z["3"].win + data.pato350z["3"].losses;
-    document.getElementById('patomatchestg').textContent = data.dicopato["4"].win + data.dicopato["4"].losses + data.dicopatito["4"].win + data.dicopatito["4"].losses + data.pato350z["4"].win + data.pato350z["4"].losses;
+    document.getElementById('patorating1v1').textContent = data.dicopato["3"].rating;
+    document.getElementById('patoratingtg').textContent = data.dicopato["4"].rating;
+    document.getElementById('patomatches').textContent = data.dicopato["4"].win + data.dicopato["4"].losses + data.dicopato["3"].win + data.dicopato["3"].losses;
+    document.getElementById('patomatches1v1').textContent = data.dicopato["3"].win + data.dicopato["3"].losses;
+    document.getElementById('patomatchestg').textContent = data.dicopato["4"].win + data.dicopato["4"].losses;
+    document.getElementById('dicopatitorating1v1').textContent = data.dicopatito["3"].rating;
+    document.getElementById('dicopatitoratingtg').textContent = data.dicopatito["4"].rating;
+    document.getElementById('dicopatitomatches').textContent = data.dicopatito["4"].win + data.dicopatito["4"].losses + data.dicopatito["3"].win + data.dicopatito["3"].losses;
+    document.getElementById('dicopatitomatches1v1').textContent = data.dicopatito["3"].win + data.dicopatito["3"].losses;
+    document.getElementById('dicopatitomatchestg').textContent = data.dicopatito["4"].win + data.dicopatito["4"].losses;
+    document.getElementById('pato350zrating1v1').textContent = data.pato350z["3"].rating;
+    document.getElementById('pato350zratingtg').textContent = data.pato350z["4"].rating;
+    document.getElementById('pato350zmatches').textContent = data.pato350z["4"].win + data.pato350z["4"].losses + data.pato350z["3"].win + data.pato350z["3"].losses;
+    document.getElementById('pato350zmatches1v1').textContent = data.pato350z["3"].win + data.pato350z["3"].losses;
+    document.getElementById('pato350zmatchestg').textContent = data.pato350z["4"].win + data.pato350z["4"].losses;
     document.getElementById('nanoxrating1v1').textContent = data.nanox["3"].rating;
     document.getElementById('nanoxratingtg').textContent = data.nanox["4"].rating;
     document.getElementById('nanoxmatches').textContent = data.nanox["4"].win + data.nanox["4"].losses + data.nanox["3"].win + data.nanox["3"].losses;
@@ -21,11 +31,21 @@ fetch(dataFile)
     document.getElementById('sir_monkeymatches').textContent = data.sir_monkey["4"].win + data.sir_monkey["4"].losses + data.sir_monkey["3"].win + data.sir_monkey["3"].losses;
     document.getElementById('sir_monkeymatches1v1').textContent = data.sir_monkey["3"].win + data.sir_monkey["3"].losses;
     document.getElementById('sir_monkeymatchestg').textContent = data.sir_monkey["4"].win + data.sir_monkey["4"].losses;
-    document.getElementById('alanthekatrating1v1').textContent = Math.max(data.alanthekat["3"].rating, data.carpincho["3"].rating, data.thexcarpincho["3"].rating);
-    document.getElementById('alanthekatratingtg').textContent = Math.max(data.alanthekat["4"].rating, data.carpincho["4"].rating, data.thexcarpincho["4"].rating);
-    document.getElementById('alanthekatmatches').textContent = data.alanthekat["4"].win + data.alanthekat["4"].losses + data.alanthekat["3"].win + data.alanthekat["3"].losses + data.carpincho["4"].win + data.carpincho["4"].losses + data.carpincho["3"].win + data.carpincho["3"].losses + data.thexcarpincho["4"].win + data.thexcarpincho["4"].losses + data.thexcarpincho["3"].win + data.thexcarpincho["3"].losses;
-    document.getElementById('alanthekatmatches1v1').textContent = data.alanthekat["3"].win + data.alanthekat["3"].losses + data.carpincho["3"].win + data.carpincho["3"].losses + data.thexcarpincho["3"].win + data.thexcarpincho["3"].losses;
-    document.getElementById('alanthekatmatchestg').textContent = data.alanthekat["4"].win + data.alanthekat["4"].losses + data.carpincho["4"].win + data.carpincho["4"].losses + data.thexcarpincho["4"].win + data.thexcarpincho["4"].losses;
+    document.getElementById('alanthekatrating1v1').textContent = data.alanthekat["3"].rating;
+    document.getElementById('alanthekatratingtg').textContent = data.alanthekat["4"].rating;
+    document.getElementById('alanthekatmatches').textContent = data.alanthekat["4"].win + data.alanthekat["4"].losses + data.alanthekat["3"].win + data.alanthekat["3"].losses;
+    document.getElementById('alanthekatmatches1v1').textContent = data.alanthekat["3"].win + data.alanthekat["3"].losses;
+    document.getElementById('alanthekatmatchestg').textContent = data.alanthekat["4"].win + data.alanthekat["4"].losses;
+    document.getElementById('carpinchorating1v1').textContent = data.carpincho["3"].rating;
+    document.getElementById('carpinchoratingtg').textContent = data.carpincho["4"].rating;
+    document.getElementById('carpinchomatches').textContent = data.carpincho["4"].win + data.carpincho["4"].losses + data.carpincho["3"].win + data.carpincho["3"].losses;
+    document.getElementById('carpinchomatches1v1').textContent = data.carpincho["3"].win + data.carpincho["3"].losses;
+    document.getElementById('carpinchomatchestg').textContent = data.carpincho["4"].win + data.carpincho["4"].losses;
+    document.getElementById('thexcarpinchorating1v1').textContent = data.thexcarpincho["3"].rating;
+    document.getElementById('thexcarpinchoratingtg').textContent = data.thexcarpincho["4"].rating;
+    document.getElementById('thexcarpinchomatches').textContent = data.thexcarpincho["4"].win + data.thexcarpincho["4"].losses + data.thexcarpincho["3"].win + data.thexcarpincho["3"].losses;
+    document.getElementById('thexcarpinchomatches1v1').textContent = data.thexcarpincho["3"].win + data.thexcarpincho["3"].losses;
+    document.getElementById('thexcarpinchomatchestg').textContent = data.thexcarpincho["4"].win + data.thexcarpincho["4"].losses;
     document.getElementById('emorating1v1').textContent = data.emo["3"].rating;
     document.getElementById('emoratingtg').textContent = data.emo["4"].rating;
     document.getElementById('emomatches').textContent = data.emo["4"].win + data.emo["4"].losses + data.emo["3"].win + data.emo["3"].losses;
@@ -33,48 +53,24 @@ fetch(dataFile)
     document.getElementById('emomatchestg').textContent = data.emo["4"].win + data.emo["4"].losses;
 
    
-    var datescarpincho=[]
-    datescarpincho.push(moment(data.alanthekat["4"].lastmatchdate, 'DD MM YYYY').toDate());
-    datescarpincho.push(moment(data.alanthekat["3"].lastmatchdate, 'DD MM YYYY').toDate());
-    datescarpincho.push(moment(data.carpincho["4"].lastmatchdate, 'DD MM YYYY').toDate());
-    datescarpincho.push(moment(data.carpincho["3"].lastmatchdate, 'DD MM YYYY').toDate());
-    datescarpincho.push(moment(data.thexcarpincho["4"].lastmatchdate, 'DD MM YYYY').toDate());
-    datescarpincho.push(moment(data.thexcarpincho["3"].lastmatchdate, 'DD MM YYYY').toDate());
-    var maxDatecarpincho=new Date(Math.max.apply(null,datescarpincho)); 
-    var formattedDatecarpincho = moment(maxDatecarpincho).format('DD/MM/YYYY');
-    document.getElementById('alanthekatrecentmatchdate').textContent = formattedDatecarpincho;
+    function latestMatchDate(player) {
+      var dates = [
+        moment(player["3"].lastmatchdate, 'DD MM YYYY').toDate(),
+        moment(player["4"].lastmatchdate, 'DD MM YYYY').toDate()
+      ];
+      var maxDate = new Date(Math.max.apply(null, dates));
+      return moment(maxDate).format('DD/MM/YYYY');
+    }
 
-    var datesdicopatito=[]
-    datesdicopatito.push(moment(data.dicopato["4"].lastmatchdate, 'DD MM YYYY').toDate());
-    datesdicopatito.push(moment(data.dicopato["3"].lastmatchdate, 'DD MM YYYY').toDate());
-    datesdicopatito.push(moment(data.dicopatito["4"].lastmatchdate, 'DD MM YYYY').toDate());
-    datesdicopatito.push(moment(data.dicopatito["3"].lastmatchdate, 'DD MM YYYY').toDate());
-    datesdicopatito.push(moment(data.pato350z["4"].lastmatchdate, 'DD MM YYYY').toDate());
-    datesdicopatito.push(moment(data.pato350z["3"].lastmatchdate, 'DD MM YYYY').toDate());
-    var maxDatedicopatito=new Date(Math.max.apply(null,datesdicopatito)); 
-    var formattedDatedicopatito = moment(maxDatedicopatito).format('DD/MM/YYYY');
-    document.getElementById('dicopatitorecentmatchdate').textContent = formattedDatedicopatito;
-
-    var datessir_monkey=[]
-    datessir_monkey.push(moment(data.sir_monkey["4"].lastmatchdate, 'DD MM YYYY').toDate());
-    datessir_monkey.push(moment(data.sir_monkey["3"].lastmatchdate, 'DD MM YYYY').toDate());
-    var maxDatesir_monkey=new Date(Math.max.apply(null,datessir_monkey)); 
-    var formattedDatesir_monkey =moment(maxDatesir_monkey).format('DD/MM/YYYY');
-    document.getElementById('sirmonkeyrecentmatchdate').textContent = formattedDatesir_monkey;
-
-    var datesnanox=[]
-    datesnanox.push(moment(data.nanox["4"].lastmatchdate, 'DD MM YYYY').toDate());
-    datesnanox.push(moment(data.nanox["3"].lastmatchdate, 'DD MM YYYY').toDate());
-    var maxDatenanox=new Date(Math.max.apply(null,datesnanox)); 
-    var formattedDatenanox = moment(maxDatenanox).format('DD/MM/YYYY');
-    document.getElementById('nanoxrecentmatchdate').textContent = formattedDatenanox;
-
-    var datesemo=[]
-    datesemo.push(moment(data.emo["4"].lastmatchdate, 'DD MM YYYY').toDate());
-    datesemo.push(moment(data.emo["3"].lastmatchdate, 'DD MM YYYY').toDate());
-    var maxDateemo=new Date(Math.max.apply(null,datesemo)); 
-    var formattedDateemo = moment(maxDateemo).format('DD/MM/YYYY');
-    document.getElementById('emorecentmatchdate').textContent = formattedDateemo;
+    document.getElementById('alanthekatrecentmatchdate').textContent = latestMatchDate(data.alanthekat);
+    document.getElementById('carpinchorecentmatchdate').textContent = latestMatchDate(data.carpincho);
+    document.getElementById('thexcarpinchorecentmatchdate').textContent = latestMatchDate(data.thexcarpincho);
+    document.getElementById('dicopatorecentmatchdate').textContent = latestMatchDate(data.dicopato);
+    document.getElementById('dicopatitorecentmatchdate').textContent = latestMatchDate(data.dicopatito);
+    document.getElementById('pato350zrecentmatchdate').textContent = latestMatchDate(data.pato350z);
+    document.getElementById('sirmonkeyrecentmatchdate').textContent = latestMatchDate(data.sir_monkey);
+    document.getElementById('nanoxrecentmatchdate').textContent = latestMatchDate(data.nanox);
+    document.getElementById('emorecentmatchdate').textContent = latestMatchDate(data.emo);
 
   })
   
